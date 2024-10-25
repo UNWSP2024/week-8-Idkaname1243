f07205be45cc6d383767b7e10332a1a8685afa2a
Idkaname1243 <anderskjelland7@gmail.com>
2024-10-25 12:52:00 -0500

    Update program_1.py
    added logic to program_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,6 @@
 # Program #1: Initials
-# Write a program that gets a string containing a person's first, middle, and last names, 
-# and displays their first, middle, and last initials.  
+# Write a program that gets a string containing a person's first, middle, and last names,
+# and displays their first, middle, and last initials.
 # For example, if the user enters John William Smith, the program should display J. W. S.
 
 # Add your logic starting on line 11
@@ -9,8 +9,9 @@ def initials_generator(personsName):
 
     personsInitials = ""
     #    Add your logic here
-
-    return personsInitials.strip()
+    words = personsName.split()
+    initials = '.'.join(word[0] for word in words) + '.'
+    return initials
 
 personsName = input('Enter the users first, middle, and last name')
 
