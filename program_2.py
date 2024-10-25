1323616068c54ba4a18c9a2fc87b4e291c678482
Idkaname1243 <anderskjelland7@gmail.com>
2024-10-25 13:27:30 -0500

    Update program_2.py
    added logic for program_2.py

--- program_2.py
+++ program_2.py
@@ -1,8 +1,8 @@
 # Program #2: Word Separator
-# Write a program that accepts as input a sentence in which all of the words are run together, 
-# but the first character of each word is uppercase.  
-# Convert the sentence to a string in which the words are separated by spaces, 
-# and the first word starts with an uppercase.  
+# Write a program that accepts as input a sentence in which all of the words are run together,
+# but the first character of each word is uppercase.
+# Convert the sentence to a string in which the words are separated by spaces,
+# and the first word starts with an uppercase.
 # For example the string "StopAndSmellTheRoses" would be converted to "Stop and smell the roses."
 
 # Start your changes on line 13
@@ -11,8 +11,16 @@ def word_separator(sentence):
 
     new_sentence = ""
     #    Add your logic here
+    for char in sentence:
+        if char.isupper():
+            new_sentence += " "
+        new_sentence += char
+    new_sentence = new_sentence.lower()
+    new_sentence = new_sentence.split()
+    if new_sentence:
+        new_sentence[0]=new_sentence[0].capitalize()
 
-    return new_sentence.strip()
+    return ' '.join(new_sentence) #.strip()
 
 # Example usage
 
